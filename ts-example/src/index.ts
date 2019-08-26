cf1bcda5e5ea9c56b9ec86732196db7a7c480201
MoonCheung <salvador23@163.com>
2019-08-27 00:11:52 +0800

    create example

---
+++ ts-example/src/index.ts
@@ -0,0 +1,11 @@
+/*
+ * @Description: 基本例子
+ * @Author: MoonCheung
+ * @Github: https://github.com/MoonCheung
+ * @Date: 2019-08-26 14:52:09
+ * @LastEditors: MoonCheung
+ * @LastEditTime: 2019-08-26 15:44:49
+ */
+let hello = 'hello typescript';
+
+document.querySelectorAll('.app')[0].innerHTML = hello;
