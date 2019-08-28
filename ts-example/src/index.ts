de53fda3c278e35b17573ed2cfdfde7294b49aa0
MoonCheung <salvador23@163.com>
2019-08-28 15:59:41 +0800

    add enum & interface

--- ts-example/src/index.ts
+++ ts-example/src/index.ts
@@ -4,8 +4,8 @@
  * @Github: https://github.com/MoonCheung
  * @Date: 2019-08-26 14:52:09
  * @LastEditors: MoonCheung
- * @LastEditTime: 2019-08-26 15:44:49
+ * @LastEditTime: 2019-08-28 13:50:25
  */
-let hello = 'hello typescript';
 
+let hello: string = 'Hello TypeScript';
 document.querySelectorAll('.app')[0].innerHTML = hello;
