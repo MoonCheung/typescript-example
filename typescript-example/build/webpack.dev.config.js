397e790f5a082c563fb0862bce289eab7234a978
MoonCheung <salvador23@163.com>
2020-06-08 01:35:16 +0800

    feat: webpack devServer

--- typescript-example/build/webpack.dev.config.js
+++ typescript-example/build/webpack.dev.config.js
@@ -1,3 +1,11 @@
+const path = require('path');
+
 module.exports = {
-  devtool: 'cheap-module-eval-source-map'
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, '../src'),
+    hot: false, // 开启热更新
+    stats: 'errors-only',
+    inline: true
+  }
 };
