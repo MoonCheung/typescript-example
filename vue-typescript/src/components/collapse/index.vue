11361926ed367329f8ba6cc705cdca083aef6039
MoonCheung <salvador23@163.com>
2020-10-06 01:28:38 +0800

    chore: 手风琴代码

---
+++ vue-typescript/src/components/collapse/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="wrap">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Collapse',
+  methods: {
+    cut(childId) {
+      this.$children.forEach(child => {
+        console.log('ci', child._uid);
+        if (child._uid !== childId) {
+          child.show = false;
+        }
+      });
+    }
+  }
+});
+</script>
---
+++ vue-typescript/src/components/collapseItem/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="title" @click="change">{{ title }}</div>
+    <div v-show="show">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CollapseItem',
+  props: ['title'],
+  data() {
+    return {
+      show: false
+    };
+  },
+  methods: {
+    change() {
+      console.log('手风琴子组件:', this._uid);
+      this.$parent.cut(this._uid);
+      this.show = !this.show;
+    }
+  }
+});
+</script>
