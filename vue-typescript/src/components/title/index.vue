9683870bbb71e55500d281dd14d8586ebc746e5f
MoonCheung <salvador23@163.com>
2020-10-08 14:50:47 +0800

    feat: slot标题

---
+++ vue-typescript/src/components/title/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h4 class="titleName">
+      <!-- 插槽内容 -->
+      <slot></slot>
+    </h4>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TitleName'
+});
+</script>
