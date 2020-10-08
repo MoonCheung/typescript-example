14b938ab255c38c69748de57b414fe0a128d196a
MoonCheung <salvador23@163.com>
2020-10-08 14:40:06 +0800

    feat: $emit 实例

---
+++ vue-typescript/src/components/event_cmpt/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    name:{{ name || '默认值为空' }}<br />
+    <input :value="name" @input="handleChange($event)" />
+    <button @click="handleChickReset">重置成功</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, toRefs } from 'vue';
+
+export default defineComponent({
+  name: 'childEvent',
+  props: {
+    name: {
+      type: [String, Object]
+    }
+  },
+  methods: {
+    handleChange(event: any) {
+      // 阻止捕获或者冒泡阶段中当前事件的进一步传播
+      event.stopPropagation();
+      this.$emit('change', event);
+    },
+    handleChickReset(event: any) {
+      // 阻止捕获或者冒泡阶段中当前事件的进一步传播
+      event.stopPropagation();
+      document.getElementsByTagName('input')[0].value = '';
+      this.$emit('clear');
+    }
+  }
+});
+</script>
---
+++ vue-typescript/src/views/Basis/event.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <title-name>$emit&$on(废弃) 实例</title-name>
+    <child-event :name="name" @change="handleEventChange" @clear="handleClearInput" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import titleName from '@/components/title/index.vue';
+import childEvent from '@/components/event_cmpt/index.vue';
+
+export default defineComponent({
+  name: 'Event',
+  components: {
+    childEvent,
+    titleName
+  },
+  setup() {
+    const name = ref<string>('');
+
+    const handleEventChange = (event: any): void => {
+      name.value = event.target.value;
+    };
+
+    const handleClearInput = (): void => {
+      name.value = '';
+    };
+
+    return {
+      name,
+      handleEventChange,
+      handleClearInput
+    };
+  }
+});
+</script>
