dbd4438d0e8648a7b4e8f2ab24e1921765d58748
MoonCheung <salvador23@163.com>
2020-10-08 14:41:13 +0800

    chore: 增减量

---
+++ vue-typescript/src/views/Basis/IncAndDec.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <title-name>这是增减按钮</title-name>
+    <button @click="() => count++">增加</button>
+    <span style="margin: 0 10px">{{ count }}</span>
+    <button @click="() => count--" :disabled="count === 0">减少</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import titleName from '@/components/title/index.vue';
+
+export default defineComponent({
+  name: 'IncAndDecs',
+  components: {
+    titleName
+  },
+  setup() {
+    const count = ref<number>(1);
+
+    return {
+      count
+    };
+  }
+});
+</script>
